9609bfec3785ccfc8e009b7d8784f11edbfbbfdb
Dolphay1 <ryan.thornton649@gmail.com>
2026-02-20 03:42:24 -0500

    Added the img parser from the newer htmlparser version.

--- format/htmlparser.go
+++ format/htmlparser.go
@@ -62,6 +62,7 @@ type LinkConverter func(text, href string, ctx Context) string
 type ColorConverter func(text, fg, bg string, ctx Context) string
 type CodeBlockConverter func(code, language string, ctx Context) string
 type PillConverter func(displayname, mxid, eventID string, ctx Context) string
+type ImageConverter func(src, alt, title, width, height string, isEmoji bool) string
 
 func DefaultPillConverter(displayname, mxid, eventID string, _ Context) string {
 	switch {
@@ -99,6 +100,7 @@ type HTMLParser struct {
 	MonospaceBlockConverter CodeBlockConverter
 	MonospaceConverter      TextConverter
 	TextConverter           TextConverter
+	ImageConverter          ImageConverter
 }
 
 // TaggedString is a string that also contains a HTML tag.
@@ -270,6 +272,19 @@ func (parser *HTMLParser) blockquoteToString(node *html.Node, ctx Context) strin
 	return strings.Join(childrenArr, "\n")
 }
 
+func (parser *HTMLParser) imgToString(node *html.Node, ctx Context) string {
+	src := parser.getAttribute(node, "src")
+	alt := parser.getAttribute(node, "alt")
+	title := parser.getAttribute(node, "title")
+	width := parser.getAttribute(node, "width")
+	height := parser.getAttribute(node, "height")
+	_, isEmoji := parser.maybeGetAttribute(node, "data-mx-emoticon")
+	if parser.ImageConverter != nil {
+		return parser.ImageConverter(src, alt, title, width, height, isEmoji)
+	}
+	return alt
+}
+
 func (parser *HTMLParser) linkToString(node *html.Node, ctx Context) string {
 	str := parser.nodeToTagAwareString(node.FirstChild, ctx)
 	href := parser.getAttribute(node, "href")
@@ -311,6 +326,8 @@ func (parser *HTMLParser) tagToString(node *html.Node, ctx Context) string {
 		return parser.nodeToTagAwareString(node.FirstChild, ctx)
 	case "hr":
 		return parser.HorizontalLine
+	case "img":
+		return parser.imgToString(node, ctx)
 	case "pre":
 		var preStr, language string
 		if node.FirstChild != nil && node.FirstChild.Type == html.ElementNode && node.FirstChild.Data == "code" {
